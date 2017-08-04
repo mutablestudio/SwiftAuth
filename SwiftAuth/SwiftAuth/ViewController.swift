6bf7e9c610d64019318e5d4a5bc57c11e8036781
mutablestudio <anthony@mutable.co>
2017-08-04 19:21:06 +0200

    init

---
+++ SwiftAuth/SwiftAuth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftAuth
+//
+//  Created by Anthony Fresina on 8/4/17.
+//  Copyright © 2017 Mutable LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
